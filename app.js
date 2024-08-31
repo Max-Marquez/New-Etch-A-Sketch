80f32e23ee59364eed5b9847e7feb606eb30c998
w3cj <maxmarquezjr@gmail.com>
2024-08-30 18:38:30 -0700

    Added Reset Button functionality, random color on mouseover event, updated styles, added id to button

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ const container = document.querySelector(".container");
 
 // SET initial layout with 16x16 grid
 
-var canvas = document.getElementById('gridContainer');
-var width = canvas.offsetWidth;
-var height = canvas.offsetHeight;
+const canvas = document.getElementById('gridContainer');
+const width = canvas.offsetWidth;
+const height = canvas.offsetHeight;
 
 console.log(width, height);
 
+// create function to randomize grid box background color on hover
+
+function randomColor() {
+  let color = [];
+  for (let i = 0; i < 3; i++) {
+    color.push(Math.floor(Math.random() * 256));
+  }
+  return 'rgb(' + color.join(', ') + ')';
+}
 
 function createGrid(boxesPerRow) {
     // create row
@@ -24,7 +33,7 @@ function createGrid(boxesPerRow) {
             const box = document.createElement('div');
             box.classList.add('column');
             box.addEventListener("mouseover", (event) => {
-                box.style.backgroundColor = '#0000FF';
+                box.style.backgroundColor = randomColor();
             });
             row.appendChild(box);
         }
@@ -33,3 +42,29 @@ function createGrid(boxesPerRow) {
 }
 
 createGrid(16);
+
+
+// Add eventListener to the button
+// Store prompt value as a number using parseInt() (prompt stores as string)
+// Function should call createGrid(boxesPerRow) to create new grid
+
+// ERASE previous boxes so that the grid can be reset
+function resetGrid(){
+  // Target the div boxes
+  //e.firstElementChild can be used. 
+        let child = canvas.lastElementChild;
+        while (child) {
+            canvas.removeChild(child);
+            child = canvas.lastElementChild;
+        }
+};
+
+// GET userBoxesPerRow using prompt method and store that value as an integer
+  const button = document.getElementById("resetButton");
+  button.addEventListener("click", function() {
+  let userInput = parseInt(prompt("Enter a number for the grid size. For Example: 64 will produce a 64x64 grid"));
+    console.log(userInput);
+    // create another function to remove the current grid children and then reset the grid size
+    resetGrid();
+    createGrid(userInput);
+  });
